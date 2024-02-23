969cf3c3670620209c5e04836f8c0bfb6bc19d41
ParthSalunke <paarthsalunke@gmail.com>
2024-02-23 23:09:14 +0530

    read frames

--- src/01read_frames.cpp
+++ src/01read_frames.cpp
@@ -7,50 +7,50 @@
 using namespace cv;
 using namespace std;
 
-//// for Image ////
-// int main()
-// {
-//     String path = "E:/parth/opencvproject/Resources/lena.png";
-//     Mat image = imread(path);
-//     if (image.empty()) {
-//         cout << "No image data" << endl;
-//         return -1;
-//     }
-//     namedWindow("Display Image", WINDOW_AUTOSIZE);
-//     imshow("Display Image", image);
-//     waitKey(0);
-//     return 0;
-// }
+// for Image ////
+int main()
+{
+    String path = "E:/parth/opencvproject/Resources/lena.png";
+    Mat image = imread(path);
+    if (image.empty()) {
+        cout << "No image data" << endl;
+        return -1;
+    }
+    namedWindow("Display Image", WINDOW_AUTOSIZE);
+    imshow("Display Image", image);
+    waitKey(0);
+    return 0;
+}
 
 
-//// for video ////
+// for video ////
 
-// int main()
-// {
-//     String path = "E:/parth/opencvproject/Resources/test_video.mp4";
+int main()
+{
+    String path = "E:/parth/opencvproject/Resources/test_video.mp4";
 
-//     VideoCapture cap(path);
-//     Mat image;
+    VideoCapture cap(path);
+    Mat image;
 
-//     while(true){
-//         cap.read(image);
-//         imshow("Display Image", image);
-//         waitKey(1);
-//     }
+    while(true){
+        cap.read(image);
+        imshow("Display Image", image);
+        waitKey(1);
+    }
 
-// }
+}
 
-//// for web cam ////
+// for web cam ////
 
-// int main()
-// {
-//     VideoCapture cap(0);
-//     Mat image;
+int main()
+{
+    VideoCapture cap(0);
+    Mat image;
 
-//     while(true){
-//         cap.read(image);
-//         imshow("Display Image", image);
-//         waitKey(1);
-//     }
+    while(true){
+        cap.read(image);
+        imshow("Display Image", image);
+        waitKey(1);
+    }
 
-// }
+}
